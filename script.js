091982cf98034d07dc83e8ccaccbcab6051b3e75
Pliblik <pliblik@gmail.com>
2025-07-21 16:47:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 /* Desenvolva seu código abaixo */
+
+
+function playRockPaperScissor(player1,player2) {
+let resultado;
+ if (player1 == "Pedra" && player2 == "Tesoura")
+{resultado ="Jogador 1 venceu!"} 
+else if (player1 == "Pedra" && player2 == "Papel")
+ {resultado = "Jogador 2 venceu!"}
+ else if (player1 == "Pedra" && player2 == "Pedra")
+    {resultado = "Empate!"}
+ else if (player1 == "Papel" && player2 == "Pedra")
+{  resultado ="Jogador 1 venceu!"} 
+else if (player1 == "Papel" && player2 == "Tesoura")
+ {resultado= "Jogador 2 venceu!"}
+ else if (player1 == "Papel" && player2 == "Papel")
+    {resultado ="Empate!"}
+ else if (player1 == "Tesoura" && player2 == "Papel")
+{ resultado ="Jogador 1 venceu!"} 
+else if (player1 == "Tesoura" && player2 == "Pedra")
+ {resultado="Jogador 2 venceu!"}
+ else if(player1 == "Tesoura" && player2 == "Tesoura")
+    {resultado="Empate!"}
+
+
+ return (resultado);
+
+ 
+
+}
+
+let jogador1 =prompt("Pedra, Papel ou Tesoura?");
+let jogador2 =prompt("Pedra, Papel ou Tesoura?");
+
+let resultado = playRockPaperScissor(jogador1,jogador2);
+
+console.log(resultado);
+let confirmação= confirm("deseja jogar novamente?" );
+
+
+
+
+
